c2381ed4a4eaa4307ef0bf3424790b0b48872040
Tadhg McIntyre <mcintyretadhg@gmail.com>
2021-06-26 12:37:28 -0400

    created separate file for character routes

---
+++ routes/api/characters.js
@@ -0,0 +1,12 @@
+const router = require("express").Router();
+const AccountControls = require('../../controllers/controller');
+
+// add character
+// router.route("/newcharacter")
+//     .post(/* create new character controller */)
+    
+// update character
+// router.route("/character/:id")
+//     .put(/* update a character, controller */ )
+
+module.exports = router
