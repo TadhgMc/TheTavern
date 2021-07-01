f20faa36827cdb249b573ac858a6ed4b46537656
bHutchingson <69726199+bHutchingson@users.noreply.github.com>
2021-07-01 19:47:47 -0400

    linked sign up page to log in page

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import './App.css';
 import Parent from './components/Parent'
 import Navbar from "./components/navbar/index";
-import SignUp from "./components/signup/index";
+import Signup from "./pages/signup/Signup";
 import Login from "./pages/login/Login";
 import Homepage from "./pages/homepage/Homepage";
 
@@ -16,6 +16,8 @@ function App() {
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route exact path="/login" component={Login} />
+        <Route exact path="/signup" component={Signup} />
+
         <Route exact path="/charactersheet" component="" />
         <Route exact path="/charactercreater1" component="" />
         <Route exact path="/charactercreater2" component="" />
