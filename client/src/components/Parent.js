a8ca703bdf5540b4a79319db9af582c4fb8e9e83
Jesse Rutter <rutter1010@gmail.com>
2021-06-30 21:57:50 -0400

    Dynamic Input Component (Equipment)

--- client/src/components/Parent.js
+++ client/src/components/Parent.js
@@ -4,6 +4,8 @@ import Child2 from "./Child2";
 import Child3 from "./Child3";
 import Child4 from "./Child4";
 import Child5 from "./Child5";
+import Child6 from "./Child6";
+import Child7 from "./Child7";
 
 // Need to move class to models if this will be saved to DB.
 
@@ -19,12 +21,17 @@ export default class Parent extends React.Component {
         const child3 = <Child3/>
         const child4 = <Child4/>
         const child5 = <Child5/>
+        const child6 = <Child6/>
+        const child7 = <Child7/>
+
         return (
             child,
             child2,
             child3,
             child4,
-            child5
+            child5,
+            child6,
+            child7
         );
     }
 }
