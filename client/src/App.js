8909ca786ed037ed644d954daa92957857c03531
Brandon <hutchingsonb@gmail.com>
2021-07-01 18:48:45 -0400

    created landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,26 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import './App.css';
-
 import Parent from './components/Parent'
-
-
 import Navbar from "./components/navbar/index";
-import Login from "./components/login/index";
 import SignUp from "./components/signup/index";
+import Login from "./pages/login/Login";
+import Homepage from "./pages/homepage/Homepage";
 
 
 
 function App() {
   return (
-    <Router>
-      {/* <Nav /> */}
-<Parent />
-
+    <BrowserRouter>
       <Navbar />
-      <Login />
-      <SignUp />
-
       <Switch>
-        <Route exact path="/" component="" />
-        <Route exact path="/profile" component="" />
+        <Route exact path="/" component={Homepage} />
+        <Route exact path="/login" component={Login} />
         <Route exact path="/charactersheet" component="" />
         <Route exact path="/charactercreater1" component="" />
         <Route exact path="/charactercreater2" component="" />
       </Switch>
-    </Router>
+    </BrowserRouter>
   );
 }
 
