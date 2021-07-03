2b35ff3fc311f01a56c03c1c883b2ce3ad887dc0
bHutchingson <69726199+bHutchingson@users.noreply.github.com>
2021-07-03 00:37:22 -0400

    merged profile component into profile page file

--- client/src/components/profileCharCard/index.js
+++ client/src/components/profileCharCard/index.js
@@ -5,9 +5,9 @@ function ProfileCharCard(props) {
   return (
     <div className="card" >
       <div className="card-body">
-        <h5 className="card-title">Character Name</h5>
-        <h6 className="card-subtitle mb-2 text-muted">Level 7</h6>
-        <p className="card-text">Tiefling | Rogue </p>
+        <h5 className="card-title">{props.charName}</h5>
+        <h6 className="card-subtitle mb-2 text-muted">Level {props.level}</h6>
+        <p className="card-text">{props.race} | {props.class} </p>
         <a id='view-character' href="#" className="card-link">View</a>
         <a id='edit-character' href="#" className="card-link">Edit</a>
         <a id='delete-character' href="#" className="card-link">Delete</a>
--- client/src/pages/profile/Profile.js
+++ client/src/pages/profile/Profile.js
@@ -3,7 +3,21 @@ import ProfileCharCard from '../../components/profileCharCard/index'
 
 function Profile() {
   return (
-    <ProfileCharCard />
+    <div className="container row">
+      <div className="vw-25 text-center">
+        <h1>Profile</h1>
+      </div>
+      <div className="col text-center">
+        <button type="button" class="btn btn-primary btn-lg">Add Character</button>
+
+      </div>
+
+      <div className="col text-center">
+        <h2>Characters:</h2>
+        <ProfileCharCard charName={"Jim"} level={69} race={'Tiefling'} class={'Rogue'} />
+        <ProfileCharCard charName={"Charles"} level={420} race={'Halfling'} class={'Bard'} />
+      </div>
+    </div>
   )
 }
 
