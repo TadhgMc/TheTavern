a3269777f33cf9448a13980f52043c70c26a0f6d
William Farnsworth <williamcoreyf@gmail.com>
2021-07-03 10:12:00 -0400

    Centered elements on the sign up page

--- client/src/pages/signup/Signup.js
+++ client/src/pages/signup/Signup.js
@@ -32,25 +32,29 @@ const signupFormSubmit = (event) => {
 
 const SignUp = () => {
   return (
-    <form>
-      <h1 className="h3 mb-3 fw-normal">Sign Up</h1>
-      <div className="form-floating">
+    <form className = "container ">
+      <div className = "text-center row mb-3" style = {{margin: "15vw 0vw 15vw 0vw"}}>
+        <h1 className="h3 mb-3 fw-normal">Sign Up</h1>
+      </div>
+      <div className="form-floating row mb-3">
         <input type="text" className="form-control" id="username-signup" placeholder="name@example.com"/>
         <label for="floatingInput">Username</label>
       </div>
-      <div className="form-floating">
+      <div className="form-floating row mb-3">
         <input type="email" className="form-control" id="email-signup" placeholder="name@example.com"/>
         <label for="floatingInput">Email</label>
       </div>
-      <div className="form-floating">
+      <div className="form-floating row mb-3">
         <input type="password" className="form-control" id="password-signup" placeholder="Password"/>
         <label for="floatingPassword">Password</label>
       </div>
-      <div className="form-floating">
+      <div className="form-floating row mb-3">
         <input type="password" className="form-control" id="password-signup-validation" placeholder="Password"/>
         <label for="floatingPassword">Re-Enter Password</label>
       </div>
-      <button onClick={signupFormSubmit} className="w-100 btn btn-lg btn-primary" type="submit">Sign Up</button>
+      <div className = "row mb-3 justify-content-center">
+        <button onClick={signupFormSubmit} className="w-50 btn btn-lg btn-primary" type="submit">Sign Up</button>
+      </div>
     </form>
   )
 }
