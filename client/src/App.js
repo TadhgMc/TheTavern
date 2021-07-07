1d2b7553ab4eb58a1f0a7f4b6adadc20f924875d
bHutchingson <69726199+bHutchingson@users.noreply.github.com>
2021-07-06 21:44:46 -0400

    created a function to create a new character and get the id for the newly created character

--- client/src/App.js
+++ client/src/App.js
@@ -22,7 +22,7 @@ function App() {
         <Route exact path="/profile" component={Profile} />
         <Route path="/charactersheet/:id" component={CharacterSheet} />
         <Route exact path="/charactercreater2" component="" />
-        <Route exact path="/charactercreation" component={CharacterCreation} />
+        <Route path="/charactercreation/:id" component={CharacterCreation} />
       </Switch>
     </BrowserRouter>
   );
