6037ad6b520ff3327687bd5ce913d54b9ff91d96
bHutchingson <69726199+bHutchingson@users.noreply.github.com>
2021-07-08 11:39:30 -0400

    updated profile page to map characters properly

--- client/src/components/profileCharCard/index.js
+++ client/src/components/profileCharCard/index.js
@@ -6,13 +6,13 @@ const deleteChar = (id) => {
   API.removeCharacter(id)
 }
 
-const viewChar = (id) => {
+/* const viewChar = (id) => {
   document.location.replace('/charactersheet/' + id);
 }
 
 const editChar = (id) => {
   document.location.replace('/charactercreation/' + id);
-}
+} */
 
 
 
@@ -25,9 +25,9 @@ function ProfileCharCard(props) {
         <h6 className="card-subtitle mb-2 text-muted">Level {props.level}</h6>
         <p className="card-text">{props.race} | {props.class} </p>
         <div className='btn-container'>
-          <button type="button" onClick={viewChar(props.charId)} id='view-character' className="btn btn-primary btn-sm">View</button>
-          <button type="button" onClick={editChar(props.charId)} id='edit-character' className="btn btn-primary btn-sm">Edit</button>
-          <button type="button" onClick={deleteChar(props.charId)} id='delete-character' className="btn btn-secondary btn-sm">Delete</button>
+          <button type="button" /* onClick={viewChar(props.charId)} */ id='view-character' className="btn btn-primary btn-sm">View</button>
+          <button type="button" /* onClick={editChar(props.charId)} */ id='edit-character' className="btn btn-primary btn-sm">Edit</button>
+          <button type="button" /* onClick={deleteChar(props.charId)} */ id='delete-character' className="btn btn-secondary btn-sm">Delete</button>
         </div>
 
       </div>
